d619dfbd50004d08474c5d7323a2e1eae45ba790
Denkolaben <33037707+Denkolaben@users.noreply.github.com>
2017-12-09 02:56:03 +0300

    Исправление замечаний 1 проверки

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
-     var form = document.querySelector(".hotel-search-form");
+var form = document.querySelector(".hotel-search-form");
+var form_visible = document.querySelector(".form-visible");
+var form_validation = document.querySelector("form-visible-animation");
+var start_date = document.querySelector("[name=start-date]");
+var finish_date = document.querySelector("[name=finish-date]");
 
-     var form_visible = document.querySelector(".form-visible")
+form.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form_visible.classList.toggle("form");
+  form_visible.classList.toggle("form-visible-animation");
+ });
 
-     var form_validation = document.querySelector("form-visible-animation")
+form_visible.addEventListener("submit", function (evt) {
+  if (!start_date.value || !finish_date.value) {
+  evt.preventDefault ();
+  form_visible.classList.remove("form-error");
+  form_visible.offsetWidth = form_visible.offsetWidth;
+  form_visible.classList.add("form-error");
+  }
+ });
 
-     var start_date = document.querySelector("[name=start-date]");
-
-     var finish_date = document.querySelector("[name=finish-date]");
-
-     form.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      form_visible.classList.toggle("form");
-      form_visible.classList.toggle("form-visible-animation");
-     });
-
-    form_visible.addEventListener("submit", function (evt) {
-      if (!start_date.value || !finish_date.value) {
-      evt.preventDefault ();
-      form_visible.classList.remove("form-error");
-      form_visible.offsetWidth = form_visible.offsetWidth;
-      form_visible.classList.add("form-error");
-      }
-     });
-
-      form_visible.classList.toggle("form-visible");
-      form_visible.classList.toggle("form");
+  form_visible.classList.toggle("form-visible");
+  form_visible.classList.toggle("form");
